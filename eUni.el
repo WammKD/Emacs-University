2ca46bda5f3773fc716169eddc7778f17ff5f699
WammKD <jaft.r@outlook.com>
2016-12-05 23:08:08 -0600

    Added constants for the names of the three buffers and expanded eUni-start to accept strings to insert as contents into the Explanations and Lesson windows/buffers.

--- eUni.el
+++ eUni.el
@@ -1,10 +1,30 @@
-(defun eUni-start ()
+(defconst eUni-instruction-buffer-name "*Instructions*"
+  "The name of the buffer/window that will hold the current instructions.")
+(defconst eUni-lesson-buffer-name "*Lesson Name*"
+  "The name of the buffer/window that will hold where the lesson will be written.")
+(defconst eUni-evaluation-buffer-name "*Evaluations*"
+  "The name of the buffer/window that will hold the evaluated results of the user's code.")
+
+(defun eUni-start (instructions lesson)
   (interactive)
+
+  (defun switch-erase-insert-toBeg (name contents)
+    (switch-to-buffer name)
+    (erase-buffer)
+    (insert contents)
+    (beginning-of-buffer))
 
   (delete-other-windows)
 
+  (switch-erase-insert-toBeg eUni-instruction-buffer-name instructions)
+  (setq buffer-read-only t)
   (split-window-horizontally (floor (* .27 (window-width))))
+  (turn-on-visual-line-mode)
 
   (other-window 1)
 
-  (split-window-vertically (floor (* .75 (window-height)))))
+  (switch-to-buffer eUni-evaluation-buffer-name)
+  (setq buffer-read-only t)
+  (split-window-vertically (floor (* .75 (window-height))))
+
+  (switch-erase-insert-toBeg eUni-lesson-buffer-name lesson))
