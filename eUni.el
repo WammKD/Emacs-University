61f6f754e086ec0216caaf3b3a64999cd78e6e43
WammKD <jaft.r@outlook.com>
2016-12-06 23:22:47 -0600

    Added a *very* rough function to evaluate the lesson buffer and assert whether any ruby code is correct with the asssertion and some notes on possible functions to create for lesson makers to use; should I just have them evaluate and assert against any string they want? Would providing particular functions help make things more organized? How should errors be handled? Is it important to point out what the user did wrong?

--- eUni.el
+++ eUni.el
@@ -5,6 +5,9 @@
 (defconst eUni-evaluation-buffer-name "*Evaluations*"
   "The name of the buffer/window that will hold the evaluated results of the user's code.")
 
+(setq same-window-buffer-names
+  (cons eUni-evaluation-buffer-name same-window-buffer-names))
+
 (defun eUni-start (instructions lesson)
   (interactive)
 
@@ -27,4 +30,40 @@
   (setq buffer-read-only t)
   (split-window-vertically (floor (* .75 (window-height))))
 
-  (switch-erase-insert-toBeg eUni-lesson-buffer-name lesson))
+  (switch-erase-insert-toBeg eUni-lesson-buffer-name lesson)
+  (ruby-mode))
+
+(defun eUni-RubyAssert (assert)
+  (message "Checking answers…")
+
+  (write-region
+    (concat
+      (buffer-substring-no-properties (point-min) (point-max))
+      (concat "\n\nIO.write('/tmp/crap.txt', " assert ")"))
+    nil
+    "/tmp/crap.rb")
+
+  (with-temp-buffer
+    (shell-command
+      "ruby /tmp/crap.rb"
+      t))
+
+  (write-region
+    (buffer-substring-no-properties (point-min) (point-max))
+    nil
+    "/tmp/crap.rb")
+
+  (async-shell-command "irb /tmp/crap.rb" eUni-evaluation-buffer-name)
+
+  ;; (call-process-shell-command
+  ;;   (concat "ruby " (concat 
+  ;; 		      (buffer-substring-no-properties (point-min) (point-max))
+  ;; 		      (concat "\n\nIO.write('/tmp/crap.txt', " assert ")")))
+  ;;   nil
+  ;;   "*Shell Command Output*"
+  ;;   t)
+  (string-equal
+    "true"
+    (with-temp-buffer
+      (insert-file-contents "/tmp/crap.txt")
+      (buffer-string))))
