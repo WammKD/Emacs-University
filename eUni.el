56fcd1c650f7471462a505a853dea7db358937d8
WammKD <jaft.r@outlook.com>
2016-12-08 23:04:49 -0600

    Changed my unintential global variables to local variables and added an evaluation function to do evaluation seperately from assertations.

--- eUni.el
+++ eUni.el
@@ -40,41 +40,47 @@
   (switch-erase-insert-toBeg eUni-lesson-buffer-name lesson)
   (ruby-mode))
 
-(defun eUni-generalAssert (append           beginning-of-file-name
-			   interpreted-file language-file-ext      interpreter outputer)
-  (setq language-file (concat beginning-of-file-name language-file-ext))
-  (setq users-work    (concat
-		        (buffer-substring-no-properties (point-min) (point-max))
-			"\n"))
 
-  (write-region (concat users-work append) nil language-file)
-
-  (with-temp-buffer
-    (shell-command (concat interpreter " " language-file) t))
-
-  (write-region users-work nil language-file)
-
-  (message "Checking answers…")
-  (async-shell-command (concat
-			 outputer
-			 " "
-			 language-file) eUni-evaluation-buffer-name)
-
-  (string-equal
-    "true"
-    (with-temp-buffer
-      (insert-file-contents interpreted-file)
-      (buffer-string))))
-
-(defun eUni-generalAssert-init (assert-function-symbol assert lesson-name)
-  (funcall (eUni-get-language-assert-function assert-function-symbol)
-	     assert
-	     lesson-name
-	     (concat eUni-temp-directory lesson-name)
-	     (concat beginning-of-file-name eUni-interpreted-file-ext)))
 
 (defun eUni-get-language-assert-function (symbol)
   (cdr (assoc symbol eUni-language-functions)))
+
+(defun eUni-generalAssert-init (assert-function-symbol assert lesson-name)
+  (let (beginning-of-file-name (concat eUni-temp-directory lesson-name))
+    (funcall (eUni-get-language-assert-function assert-function-symbol)
+	       assert
+	       lesson-name
+	       beginning-of-file-name
+	       (concat beginning-of-file-name eUni-interpreted-file-ext))))
+
+(defun eUni-generalAssert (append           beginning-of-file-name
+			   interpreted-file language-file-ext      interpreter)
+  (let ((language-file (concat beginning-of-file-name language-file-ext))
+	(users-work    (concat
+		         (buffer-substring-no-properties (point-min) (point-max))
+			 "\n")))
+    (write-region (concat users-work append) nil language-file)
+
+    (with-temp-buffer
+      (shell-command (concat interpreter " " language-file) t))
+
+    (write-region users-work nil language-file)
+
+    (string-equal
+      "true"
+      (with-temp-buffer
+	(insert-file-contents interpreted-file)
+	(buffer-string)))))
+
+(defun eUni-generalEvaluate (outputer lesson-name)
+  (let* ((beginning-of-file-name (concat eUni-temp-directory          lesson-name))
+	 (language-file          (concat beginning-of-file-name language-file-ext)))
+    (message "Checking answers…")
+    (async-shell-command (concat
+			   outputer
+			   " "
+			   language-file) eUni-evaluation-buffer-name)))
+
 
 (defun eUni-RubyAssert (assert lesson-name beginning-of-file-name interpreted-file)
   (eUni-generalAssert
